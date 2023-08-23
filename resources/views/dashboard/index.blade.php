e9645921f058d4b9e50b6e19949603b3ae1883b6
stepheno42 <84373037+stephen042@users.noreply.github.com>
2023-08-23 18:27:56 +0100

    lis

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -20,7 +20,7 @@
     <main id="main" class="main">
 
         <div class="pagetitle">
-            <h1>OutPut Lists for {{ Auth::user()->state }}</h1>
+            <h1> List Of Indicators : {{ Auth::user()->state }}</h1>
             <nav>
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a href="index.html">Home</a></li>
